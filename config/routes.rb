d1bcd671ede6aa93eaced26311565f9ebd2e3227
James Wordie <james.wordie@yahoo.com>
2021-03-01 15:06:47 +0000

    routes all setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: "pages#dashboard"
+  resources :decks do
+    resources :cards, only: [:new, :create, :show]
+  end
+  resources :decks [] do
+    member do
+      get :results
+    end
+  end
+  resources :cards, only: [:edit, :update, :destroy]
 end
