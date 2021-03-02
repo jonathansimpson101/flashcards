a97c580b68b07167733b5ac38f60dd6289730643
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-03-02 12:48:47 +0000

    created deck show page and updated layout on dashboard

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,16 @@
+class DecksController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @deck = Deck.find(params[:id])
+    @deck_cards = @deck.cards
+  end
+
+  def edit
+  end
+
+
+
+end
