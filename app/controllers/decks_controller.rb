13a0f4725773a3740e3b1575b22b1342dae9f4f0
matt vile <matt.vile@yahoo.co.uk>
2021-03-02 17:54:34 +0000

    add edit and update for category and name

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -8,11 +8,34 @@ class DecksController < ApplicationController
     @deck_cards = @deck.cards
   end
 
-  def edit
+  def new
+    @deck = Deck.new
+  end
+
+  def create
+    @deck = Deck.new(strong_params)
+    @deck.user = current_user
+    @deck.save
+    redirect_to deck_path(@deck)
   end
 
   def results
     @deck = Deck.find(params[:id])
   end
 
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+
+  def update
+    @deck = Deck.find(params[:id])
+    @deck.update!(strong_params)
+    redirect_to deck_path(@deck)
+  end
+
+  private
+
+  def strong_params
+    params.require(:deck).permit(:name, :category_id)
+  end
 end
