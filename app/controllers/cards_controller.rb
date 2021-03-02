9d7bbc5c45d5c69768c7fa5c8a822d5726b5b12a
James Wordie <james.wordie@yahoo.com>
2021-03-02 12:24:26 +0000

    working on but not working

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,8 @@
+class CardsController < ApplicationController
+  # def show
+  #   @deck = Deck.find(params[:deck_id])
+  #   @card = Card.find(params[:id])
+  #   @card_attempt = false
+  #   raise
+  # end
+end
