e59f671729ded29cea173dbcdc93ae02c4bef824
matt vile <matt.vile@yahoo.co.uk>
2021-03-03 16:49:56 +0000

    added delete deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -62,11 +62,11 @@ class DecksController < ApplicationController
     redirect_to deck_path(@deck)
   end
 
-  # def destroy
-  #   @deck = Deck.find(params[:id])
-  #   @deck.destroy
-  #   redirect_to dashboard_path
-  # end
+  def destroy
+    @deck = Deck.find(params[:id])
+    @deck.destroy
+    redirect_to dashboard_path
+  end
 
   private
 
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,7 +2,7 @@ class Deck < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :scores
-  has_many :topics
+  has_many :topics, dependent: :destroy
   has_many :cards, through: :topics
 
   validates :name, presence: true, uniqueness: true
