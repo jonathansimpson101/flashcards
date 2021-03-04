ae101347b914156d88241cc7da104c31bea8efdf
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-03-04 16:55:42 +0000

    change card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,6 @@ class Card < ApplicationRecord
   has_many :topics, dependent: :destroy
   has_many :decks, through: :topics
   has_many :card_scores
-  belongs_to :user
 
   validates :question, presence: true
   validates :answer, presence: true
