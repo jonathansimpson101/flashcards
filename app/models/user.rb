0870c7dc4e5f64dc8597bb80b724e53cafa4254e
James Wordie <james.wordie@yahoo.com>
2021-03-07 20:13:57 +0000

    added association to user model for scores

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :decks, dependent: :destroy
   has_one_attached :avatar
   has_many :cards, dependent: :destroy
+  has_many :scores, dependent: :destroy
 end
