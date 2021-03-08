073b0b156ecf941b78e1a2d0b9b819c4708bcddd
matt vile <matt.vile@yahoo.co.uk>
2021-03-08 14:57:05 +0000

    confetti added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "canvas-confetti"
+
 
 Rails.start()
 Turbolinks.start()
@@ -25,6 +27,7 @@ import "bootstrap";
 import { initCardListener } from "../channels/card_listner";
 import { bindFlip, scoreIncrement } from '../components/study_mode';
 import { initSelect2 } from '../components/init_select2';
+import confetti from "canvas-confetti"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
@@ -38,4 +41,52 @@ document.addEventListener('turbolinks:load', () => {
   if (! document.getElementById('deck-index-search-page')) {
       initSelect2();
   };
+const bigPercent = document.querySelector(".big-percent");
+  if (bigPercent && bigPercent.dataset.confetti ){
+
+
+var duration = 1000 * 1.5;
+var end = Date.now() + duration;
+
+(function frame() {
+  // launch a few confetti from the left edge
+  confetti({
+    particleCount: 6,
+    angle: 50,
+    spread: 80,
+    origin: { x: 0 , y:1}
+
+  });
+  // and launch a few from the right edge
+  confetti({
+    particleCount: 6,
+    angle: 130,
+    spread: 80,
+    origin: { x: 1 , y:1},
+  });
+
+  // keep going until we are out of time
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+
+
+}());
+};
+
+// var myCanvas = document.createElement('canvas');
+// document.getElementById("results-container").appendChild(myCanvas);
+
+// var myConfetti = confetti.create(myCanvas, {
+//   resize: true,
+//   useWorker: true
+// });
+// myConfetti({
+//   particleCount: 100,
+//   spread: 160
+//   // any other options from the global
+//   // confetti function
+// });
 });
+
+
