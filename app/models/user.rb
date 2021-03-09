106da93c7fc227e17ff21f8e7a355e7c24da5b48
Jonathan Simpson <jonathansimpson101@gmail.com>
2021-03-09 09:49:07 +0000

    seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
   before_update :add_streak
   has_many :decks, dependent: :destroy
-  has_one_attached :avatar
+  has_one_attached :avatar, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :scores, dependent: :destroy
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,8 @@ i=0
 
 10.times do
   user = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123456")
-  file = URI.open(avatar_images[i])
+  puts "uploading image #{i}"
+  file = URI.open("#{avatar_images[i]}")
   user.avatar.attach(io: file, filename: 'person.jpg', content_type: 'image/jpg')
   users << user
   user.save
