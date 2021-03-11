5dc6c7dec695da03871dcd8b5d525e71f2e593c3
Per Jensen <arenatofte@gmail.com>
2021-03-11 12:10:11 +0000

    decks studied this month already works BUT logins per month AND streak now working too

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
-  before_update :add_streak
+  before_update :add_streak, :add_logins_per_month
   has_many :decks, dependent: :destroy
   has_one_attached :avatar, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :scores, dependent: :destroy
 
+  def decks_studied_per_month
+    scores.where("created_at between ? and  ?", Date.today.at_beginning_of_month, Date.today + 1.day).count
+  end
+
   def add_streak
-    if (current_sign_in_at - 86_400) <= last_sign_in_at
+    if last_sign_in_at.beginning_of_day == current_sign_in_at.beginning_of_day
       # last sign in within a day therefore, no streak added
-    elsif (current_sign_in_at - 86_400) >= last_sign_in_at && (current_sign_in_at - 172_800) <= last_sign_in_at
+    elsif last_sign_in_at.beginning_of_day == current_sign_in_at.yesterday.beginning_of_day
       # last sign in is more than 24 hours but less than 48 hours therefore, adds a streak
       self.streaks += 1
     else
@@ -22,14 +26,11 @@ class User < ApplicationRecord
   end
 
   def add_logins_per_month
-    if (Date.now = Date.today.at_beginning_of_month)
-      self.logins_per_month = sign_in_count
-    else
+    if (Date.today == Date.today.at_beginning_of_month)
       self.logins_per_month = 0
+      self.sign_in_count = 0
+    else
+      self.logins_per_month = sign_in_count
     end
-  end
-
-  def decks_studied_per_month
-    scores.where("created_at between ? and  ?", Date.today.at_beginning_of_month, Date.today + 1.day).count
   end
 end
